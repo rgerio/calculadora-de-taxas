bccfc7a0b7944095aba86a3e6e7666ce142069bc
rbs7 <rbs7@cin.ufpe.br>
2020-10-19 17:06:50 -0300

    Continue the design of settings view

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {StyleSheet, View, Text, Switch} from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Main: React.FC = () => {
   // const [installments, setInstallments] = useState(0);
+  const [chargeConvenienceFee, setChargeConvenienceFee] = useState(true);
+
+  const toggleChargeConvenienceFee = useCallback(
+    () => setChargeConvenienceFee((previousState) => !previousState),
+    [],
+  );
 
   return (
     <View style={styles.container}>
@@ -17,18 +24,33 @@ const Main: React.FC = () => {
           <RNPickerSelect
             placeholder={{}}
             doneText="Pronto"
-            // useNativeAndroidPickerStyle={false}
-            style={styles.installmentsPicker}
+            useNativeAndroidPickerStyle={false}
+            style={pickerSelectStyles}
             onValueChange={(value) => console.log(value)}
             items={[
-              {label: 'Football', value: 'football'},
-              {label: 'Baseball', value: 'baseball'},
+              {label: 'Débito (1.9%)', value: '0'},
+              {label: 'Crédito - 1 parcela', value: '1'},
+              {label: 'Crédito - 2 parcela', value: '2'},
+              {label: 'Crédito - 3 parcela', value: '3'},
+              {label: 'Crédito - 4 parcela', value: '4'},
               {label: 'Hockey', value: 'hockey'},
             ]}
+            Icon={() => {
+              return <Icon name="arrow-drop-down" size={30} color="gray" />;
+            }}
           />
         </View>
-        <View>
-          <Text>Repassar Taxa?</Text>
+
+        <View style={styles.chargeConvenienceFeeContainer}>
+          <Text style={styles.chargeConvenienceFeeText}>Repassar taxa?</Text>
+          <Switch
+            style={styles.chargeConvenienceFeeSwitch}
+            // trackColor={{false: '#767577', true: '#81b0ff'}}
+            // thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+            // ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleChargeConvenienceFee}
+            value={chargeConvenienceFee}
+          />
         </View>
       </View>
     </View>
@@ -40,8 +62,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+
   priceContainer: {
-    backgroundColor: '#ffccff',
+    // backgroundColor: '#ffccff',
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: 16,
@@ -54,9 +77,50 @@ const styles = StyleSheet.create({
   priceDescription: {
     fontSize: 16,
   },
-  settingsContainer: {},
-  installmentsContainer: {},
-  installmentsPicker: {},
+
+  settingsContainer: {
+    // backgroundColor: '#bbbbff',
+    width: '100%',
+    marginTop: 16,
+  },
+
+  installmentsContainer: {
+    // backgroundColor: '#bbbbee',
+    backgroundColor: '#fff',
+    height: 64,
+    justifyContent: 'center',
+    padding: 16,
+  },
+
+  chargeConvenienceFeeContainer: {
+    // backgroundColor: '#bbffff',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 64,
+    marginTop: 8,
+    padding: 16,
+  },
+  chargeConvenienceFeeText: {
+    fontSize: 16,
+  },
+  chargeConvenienceFeeSwitch: {},
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 8,
+    paddingRight: 30,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingVertical: 0,
+    paddingHorizontal: 0,
+    color: 'black',
+    paddingRight: 30,
+  },
 });
 
 export default Main;
